d4e673c59a1a30d34f3023c77c75e2acb72a81fb
sy4102 <532160108@qq.com>
2026-02-10 21:18:08 +0800

    fix: correct variable name in Toolbar

--- demo/src/editor/Toolbar/Toolbar.tsx
+++ demo/src/editor/Toolbar/Toolbar.tsx
@@ -103,8 +103,8 @@ const Toolbar: React.FC = () => {
             </div>
 
             <ComponentImport
-                isOpen={importModalVisible}
-                onClose={() => setImportModalVisible(false)}
+                isOpen={showImport}
+                onClose={() => setShowImport(false)}
                 onSuccess={(componentName) => {
                     message.success(`组件 ${componentName} 已可用！`);
                 }}
